8a2ab626ea8b06b64a6f4fbb58e422ffec9ee9c4
ktorres63 <ktorresaro@unsa.edu.pe>
2024-03-28 18:31:54 -0500

    Add three functions to javascript

---
+++ app.js
@@ -0,0 +1,13 @@
+function chooseRock() {
+  let userSel = document.getElementById("userSel");
+  userSel.textContent = "Rock";
+}
+function choosePaper() {
+  let userSel = document.getElementById("userSel");
+  userSel.textContent = "Paper";
+}
+function chooseScissors() {
+  let userSel = document.getElementById("userSel");
+  userSel.textContent = "Scissors";
+}
+
