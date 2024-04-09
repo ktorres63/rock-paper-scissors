d102e69e86a75990c7ad309a904c57a2b603c0d1
ktorres63 <ktorresaro@unsa.edu.pe>
2024-04-08 22:28:59 -0500

    Restrict the game to 5 attempts

--- app.js
+++ app.js
@@ -3,73 +3,70 @@ const results = ["You Win 🥳", "You Lost 😥", "Draw"];
 
 let playerScore = 0;
 let computerScore = 0;
+let attempts = 0;
+
+const userSel = document.querySelector("#userSel");
+const userScore = document.querySelector("#userScore");
+const compSel = document.querySelector("#computerSel");
+const compScore = document.querySelector("#computerScore");
+const resultUI = document.querySelector("#result");
+
+const container = document.querySelector(".score");
 
 function chooseOption(e) {
   let user = e.target.id;
   let computer = getComputerChoice();
   let result = playRound(user, computer);
 
-  let userSel = document.querySelector("#userSel");
-  let userScore = document.querySelector("#userScore");
-  let compSel = document.querySelector("#computerSel");
-  let compScore = document.querySelector("#computerScore");
-  let resultUI = document.querySelector("#result");
-  
   userSel.textContent = user.toUpperCase();
   compSel.textContent = computer.toUpperCase();
 
   userScore.textContent = playerScore;
-  compScore.textContent = computerScore
-
-  resultUI.textContent = result;
-  
-  console.log(`YOU: ${user} \nIA: ${computer} \nRESULT:${result}`);
-
+  compScore.textContent = computerScore;
+  attempts++;
+  if (attempts === 5) {
+    if (userScore > computerScore) {
+      resultUI.textContent = results[0];
+    } else if (computerScore > userScore) {
+      resultUI.textContent = results[1];
+    } else {
+      resultUI.textContent = results[2];
+    }
+  }
 }
 
 const items = document.querySelectorAll(".link");
 items.forEach((i) => {
-  // and for each one we add a 'click' listener
   i.addEventListener("click", chooseOption);
 });
-
-
 
 function getComputerChoice() {
   let index = Math.floor(Math.random() * options.length);
   return options[index];
-}
-function getPlayerChoice() {
-  return playerChoice;
 }
 
 function playRound(playSel, compSel) {
   if (playSel === options[0] && compSel === options[1]) {
     //rock vs paper
     computerScore++;
-    return results[1];
   } else if (playSel == options[0] && compSel === options[2]) {
     //rock vs scissors
     playerScore++;
-    return results[0];
   } else if (playSel == options[1] && compSel === options[0]) {
     // paper vs rock
     playerScore++;
-    return results[0];
   } else if (playSel == options[1] && compSel === options[2]) {
     // paper vs scissors
     computerScore++;
-    return results[1];
   } else if (playSel == options[2] && compSel === options[0]) {
     // scissors vs rock
     computerScore++;
-    return results[1];
   } else if (playSel == options[2] && compSel === options[1]) {
     // scissors vs paper
     playerScore++;
-    return results[0];
   } else {
-    return results[2];
+    playerScore++;
+    computerScore++;
   }
 }
 
